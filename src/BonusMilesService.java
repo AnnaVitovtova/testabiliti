1d735170926992537cc93eb38104e145fe9043a9
vitovtova ann <vitovtova.anya@mail.ru>
2022-06-15 18:44:40 +0300

    first

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int priceBonus = 20;
+        int bonusMiles;
+        bonusMiles = cost / priceBonus;
+        return bonusMiles;
+    }
+}
+
+
